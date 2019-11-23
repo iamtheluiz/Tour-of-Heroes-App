e03756651536feaf66316348811e5619504d7d3d
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-11-23 03:33:16 -0300

    Add application dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Components
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 // Routes
 const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/dashboard",
+    pathMatch: "full"
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
   {
     path: "heroes",
     component: HeroesComponent
   }
 ];
 
+// Configura as informações das rotas
 @NgModule({
   exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)]
